ef021eea944b417be8cccebc14c944fa2ed555c8
sirciv <spencerallan8@gmail.com>
2021-05-13 16:43:33 -0500

    Artist cards completed for hw04

---
+++ hw04/your_task/js/scripts.js
@@ -0,0 +1,72 @@
+const baseURL = 'https://www.apitutor.org/spotify/simple/v1/search';
+
+// Note: AudioPlayer is defined in audio-player.js
+const audioFile = 'https://p.scdn.co/mp3-preview/bfead324ff26bdd67bb793114f7ad3a7b328a48e?cid=9697a3a271d24deea38f8b7fbfa0e13c';
+const audioPlayer = AudioPlayer('.player', audioFile);
+
+const search = (ev) => {
+    const term = document.querySelector('#search').value;
+    console.log('search for:', term);
+    // issue three Spotify queries at once...
+    getTracks(term);
+    getAlbums(term);
+    getArtist(term);
+    if (ev) {
+        ev.preventDefault();
+    }
+}
+
+const getTracks = (term) => {
+    console.log(`
+        get tracks from spotify based on the search term
+        "${term}" and load them into the #tracks section 
+        of the DOM...`);
+};
+
+const getAlbums = (term) => {
+    console.log(`
+        get albums from spotify based on the search term
+        "${term}" and load them into the #albums section 
+        of the DOM...`);
+};
+
+const getArtist = (term) => {
+    const elem = document.querySelector('#artist');
+    elem.innerHTML = "";
+    fetch(baseURL + '?type=artist&q=' + term)
+    .then(response => response.json())
+    .then((data) => {
+        if (data.length > 0) { //if it's not empty
+            const firstArtist = data[0];
+            elem.innerHTML += getArtistHTML(firstArtist);
+        }
+    });
+};
+
+const getArtistHTML = (data) => {
+    //please show me a maltese puppy if nothing returns
+    if (!data.image_url) {
+        data.image_url = 'https://thehappypuppysite.com/wp-content/uploads/2016/09/The-Maltese-HP-long.jpg';
+    }
+    return `<section class="artist-card" id="${data.id}">
+        <div>
+            <img src="${data.image_url}">
+            <h3>${data.name}</h3>
+            <div class="footer">
+                <a href="${data.spotify_url}" target="_blank">
+                    view on spotify
+                </a>
+            </div>
+        </div>
+    </section>`;
+};
+
+
+document.querySelector('#search').onkeyup = (ev) => {
+    // Number 13 is the "Enter" key on the keyboard
+    console.log(ev.keyCode);
+    if (ev.keyCode === 13) {
+        ev.preventDefault();
+        search();
+    }
+};
