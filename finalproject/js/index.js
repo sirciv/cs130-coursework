7d2f7caca54392e7e1ab4705c56572514d1e7e83
sirciv <spencerallan8@gmail.com>
2021-05-29 01:56:55 -0500

    Stability fixes for second deliverable

--- finalproject/js/index.js
+++ finalproject/js/index.js
@@ -1,4 +1,5 @@
-//scrolling logo code
+
+// 1. SCROLLING LOGO
 
 var gearlogo = document.getElementById("gearlogo");
 
@@ -22,3 +23,28 @@ var gearlogo = document.getElementById("gearlogo");
 window.addEventListener("optimizedScroll", function() {
     gearlogo.style.transform = "rotate("+window.pageYOffset+"deg)";
 });
+
+// 2. CONTENT SELECTION FUNCTIONS
+
+const loadChapter = (contentNumber) => {
+    let contentURL = `content/c${contentNumber}.html`;
+    console.log('loading content: '+contentURL);
+    getChapter(contentURL);
+};
+
+const getChapter = (url) => {
+    console.log('fetching chapter');
+    fetch(url)
+        .then(response => response.text())
+        .then(html => {
+            document.querySelector('.content').innerHTML = html;
+        })
+};
+
+// 3. MAPPING SIDEBAR TO CONTENT
+//TODO: This should generate automatically
+
+document.querySelector('#c1').onmouseover = loadChapter(1);
+document.querySelector('#c2').onclick = loadChapter(2);
+document.querySelector('#c3').onclick = loadChapter(3);
+
